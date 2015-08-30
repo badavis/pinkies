279050a9157b305a02569dc8285979b09545a374
badavis <bdavi015@ucr.edu>
2015-08-30 13:16:18 -0700

    Post test passed with some changes to server config and formHandler.py

--- python/formHandler.py
+++ python/formHandler.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-import html
 import cgi
 import cgitb; cgitb.enable()     # for troubleshooting
 
-print("Content-Type: text/html") # HTTP header to say HTML is following
-print()                          # blank line, end of headers
+print("Content-Type: text/html\n") # HTTP header to say HTML is following
+# end of headers
 
 form = cgi.FieldStorage()
+
+print ("<html><body><h1>Test</h1><br><h2>")
 print (form["action"].value)
+print ("</h2></body></html>")

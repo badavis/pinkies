c04651db7d3c61f7d330fd22c6a9c94e5c85db8c
Benjamin Davis <bjamin.davis@gmail.com>
2015-08-30 11:38:05 -0700

    Post/post handling
    Test to see if server receives post request from form correctly.

---
+++ python/formHandler.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import html
+import cgi
+import cgitb; cgitb.enable()     # for troubleshooting
+
+print("Content-Type: text/html") # HTTP header to say HTML is following
+print()                          # blank line, end of headers
+
+form = cgi.FieldStorage()
+print (form["action"].value)
